69c6386e94f2ba6129902f0571e2688d1b6ff9a2
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:22:10 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/libbsdport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/libbsdport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libbsdport.git'), string(name: 'PORT_DESCRIPTION', value: 'port of libbsd' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/libbsdport.git'), string(name: 'PORT_DESCRIPTION', value: 'port of libbsd' )]
   }
 }
